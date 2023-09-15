139d6996cf89e69e5dd0f94f96596f989a2deb30
aronbergman <brgmn@icloud.com>
2023-09-15 16:51:51 +0200

    feat(view): create progress view

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,30 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
     var timer = Timer()
-    var timerDictionary: [String: Int] = ["soft": 300, "medium": 450, "hard": 720]
+    var timerDictionary: [String: Int] = ["soft": 30, "medium": 450, "hard": 720]
     var secondRemainding: Int = 60
+    var persentInProgress: Float = 0 // TODO: Значения не должны мутировать
+    var hardnessIdentifier: String = "" // TODO: Значения не должны мутировать
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         
-        let hardness: String = sender.restorationIdentifier!
-        
+        hardnessIdentifier = sender.restorationIdentifier!
+        secondRemainding = timerDictionary[hardnessIdentifier]!
         timer.invalidate()
-
-        secondRemainding = timerDictionary[hardness]!
         
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: <#T##(Timer) -> Void#>)
+        timer = Timer.scheduledTimer(
+            timeInterval: 1.0,
+            target: self,
+            selector: #selector(updateTimer),
+            userInfo: nil,
+            repeats: true
+        )
     }
     
-    
-    
-    
     @objc func updateTimer() {
+        persentInProgress = currencyCalculation(
+            dividend: secondRemainding,
+            didisor: timerDictionary[hardnessIdentifier]!
+        )
+
+        progressView.progress = persentInProgress/100
+        print(persentInProgress)
         if secondRemainding > 0 {
-            print("\(secondRemainding) seconds.")
             secondRemainding -= 1
         } else {
-            
+            print("Done")
+            timer.invalidate()
+            titleLabel.setValue("Done", forKey: "text")
         }
+    }
+    
+    func currencyCalculation(dividend: Int, didisor: Int) -> Float {
+       return Float((dividend * 100)/didisor)
     }
 }
